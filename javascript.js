4ec135889bc4c33e520ea9baa1a453bf441e04e5
winditanuramaliya <winditanuramaliya@gmail.com>
2025-09-10 15:14:34 +0700

    latihan_bisdig

---
+++ javascript.js
@@ -0,0 +1,6 @@
+alert("Hai guys");
+let konfirmasi = confirm ("Aku boleh nanya sesuatu ga?");
+if (konfirmasi) {
+    let should = prompt ("Gimana kabarmu hari ini?");
+    alert("Alhamdulillah, jangan lupa bersyukur ya bub:*");
+}
